c7382a5ba37a366a0053a3aabe06c6d6c2bd62fc
Everton Ceciliano <101340199+EvertonCeciliano@users.noreply.github.com>
2025-06-10 09:37:38 -0300

    Update bot_whatsapp_automatico.py

--- bot_whatsapp_automatico.py
+++ bot_whatsapp_automatico.py
@@ -5,7 +5,7 @@ import os
 instance_id = os.environ["ULTRAMSG_INSTANCE_ID"]
 token = os.environ["ULTRAMSG_TOKEN"]
 numero = os.environ["WHATSAPP_NUMBER"]
-mensagem = "se fude"
+mensagem = "Bom dia amor! otimo dia❤️ te amo"
 
 url = f"https://api.ultramsg.com/{instance_id}/messages/chat"
 payload = {
